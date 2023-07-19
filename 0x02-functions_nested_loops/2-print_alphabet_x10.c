b8c0e0134ea60014de8eabedacc9274f081be761
Kelvin <kasperagbadon@gmail.com>
2023-07-19 06:56:13 -0700

    new updates

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,13 +4,13 @@
  * print alphabet x10 - prints 10 times the alphabet, in lowercase
  * followed by a new line
  */
- void print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
-	 
+ 
 	i = 0;
-	
+
 	while (i < 10)
 	{
 		ch = 'a';
