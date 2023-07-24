f2b55ae30119e69d8c7e5a326c2f91619b5c94d5
Kelvin <kasperagbadon@gmail.com>
2023-07-24 11:15:35 -0700

    arrays, strings, pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,7 @@
 
 /**
  * print_rev - prints a string in reverse followed by a new line
- * @*s: string to be printed in reverse
+ * @s: string to be printed in reverse
  * Return: s
  */
 void print_rev(char *s)
